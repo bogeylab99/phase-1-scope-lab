6ee5f39db537ea72dcec04a05b82465cdc7df904
kelvin <Kelvondambo@gmail.com>
2024-09-30 14:38:39 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+upperCaseCustomerName();
+
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+}
+setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'john'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'not john';
+}
+changeLeastFavoriteCustomer();
